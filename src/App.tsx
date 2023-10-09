305b5d1696b5128a20e0e6d09d401903c93c47ef
Werdeles Soares <werdelesmarcio@hotmail.com>
2023-10-08 22:12:41 -0300

    Versão 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 export function App() {
 
   return (
-    <h1>Hello World</h1>    
+    <div>
+      <button>Teste</button>
+    </div>
+        
   )
 }
 

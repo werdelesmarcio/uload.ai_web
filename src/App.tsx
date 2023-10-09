45632d7eb28e7ccc0ded89a2f1afb49915c78255
Werdeles Soares <werdelesmarcio@hotmail.com>
2023-10-08 23:33:35 -0300

    Versão 3.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react"
+import { Separator } from "./components/ui/separator";
+
 
 export function App() {
 
   return (
-    <div>
-      <button>Teste</button>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤️ no NLW da Rocketseat por Werdeles Soares
+          </span>
+
+          <Separator orientation="vertical" className="h-6"/>
+
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2"/>
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6">
+        oi
+      </main>
     </div>
         
   )
